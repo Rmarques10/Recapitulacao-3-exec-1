ee0e37c807c1f18d396527a347a7f9c6c1361706
Rafael Marques <rafaelmarques.rm86@gmail.com>
2022-10-27 12:53:34 -0300

    Recapitulação

---
+++ Recapitulacao 3 exec 1/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha 
+incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser 
+impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002 */
+
+
+int num = int.Parse(Console.ReadLine());
+
+while(num != 2002)
+{
+    Console.WriteLine("Senha Invalida");
+    num = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Acesso Permitido!");
